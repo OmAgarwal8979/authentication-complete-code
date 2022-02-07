50168ec9582770f5cd2e36381a8f903349b03c28
Om Agarwal <omsr.agarwal@gmail.com>
2022-02-07 22:46:20 +0530

    Level 4 security Added bcrypt and salting

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
 const app = express();
+const saltRounds = 10;
 
 mongoose.connect("mongodb://localhost:27017/userDB");
 
@@ -33,25 +34,35 @@ app.get("/register", function (req, res) {
 });
 
 app.post("/register", function (req, res) {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
-    newUser.save(function (err) {
-        if (err) throw err;
-        res.render("secrets");
+
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save(function (err) {
+            if (err) throw err;
+            res.render("secrets");
+        });
     });
 });
 
 app.post("/login", function (req, res) {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({ email: username }, function (err, foundUser) {
         if (err) throw err;
         else {
-            if (foundUser.password === password) {
-                res.render("secrets");
+            if (foundUser) {
+                bcrypt.compare(password, foundUser.password, function (err, result) {
+                    if (err) throw err;
+                    else {
+                        if (result == true) {
+                            res.render("secrets");
+                        }
+                    }
+                });
             }
         }
     });
